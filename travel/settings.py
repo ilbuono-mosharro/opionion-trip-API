76484b21c491d82e86fead3a4ae5c539cd342f69
Marinario <123278184+ilbuono-mosharro@users.noreply.github.com>
2023-03-15 21:33:53 +0100

    Update settings.py

--- travel/settings.py
+++ travel/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sadasdasdasdasds'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'search.apps.SearchConfig',
     'images.apps.ImagesConfig',
     # third party apps ...
-    'robots',
     'rest_framework',
     # ...
 ]
