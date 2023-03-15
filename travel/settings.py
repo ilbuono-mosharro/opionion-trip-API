b3d2e81345518e0fe76a3c5078d6bbd18fca07b2
Marinario <123278184+ilbuono-mosharro@users.noreply.github.com>
2023-03-15 21:32:16 +0100

    Revert "Merge branch 'main' of https://github.com/ilbuono-mosharro/opionion-trip-API"
    This reverts commit 313c93d30ecd29660770c9309132b6043d83d20a, reversing
    changes made to 3616099d94c2d783d6a1c1efb50aff34d881798d.

--- travel/settings.py
+++ travel/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'sadasdasdasdasds'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -43,8 +43,10 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'pages.apps.PagesConfig',
     'cities.apps.CitiesConfig',
+    'search.apps.SearchConfig',
     'images.apps.ImagesConfig',
     # third party apps ...
+    'robots',
     'rest_framework',
     # ...
 ]
