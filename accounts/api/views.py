8063bb46f1aa26a24a89aae1f5e0575adda4c4b5
Marinario <123278184+ilbuono-mosharro@users.noreply.github.com>
2023-03-19 18:01:17 +0100

    Update views.py

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,5 +1,8 @@
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework import permissions
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .permissions import IsOwner
 from .serializers import UserSerializer, UserInfoSerializer
@@ -17,3 +20,13 @@ class UserRud(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserInfoSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwner]
+
+
+class LogoutView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, format=None):
+        # Rimuovi il token dall'oggetto request dell'utente
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
